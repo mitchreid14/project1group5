7f5fcf98e6349e52d0a38a34f6649120bd453c11
accubility <joshua.obando98@gmail.com>
2020-11-13 14:30:08 -0500

    working on player functions and will add another api for test

--- assets/JS/lyrics.js
+++ assets/JS/lyrics.js
@@ -1,27 +1,28 @@
 function findLyrics() {
 
-    $.get("https://api.lyrics.ovh/v1/" + document.getElementById("artist").value + "/" + document.getElementById("title").value,
-      function (data) {
+  $.get("https://api.lyrics.ovh/v1/" + document.getElementById("artist").value + "/" + document.getElementById("title").value,
+    function (data) {
 
-        document.getElementById("result").innerHTML = data.lyrics.replace(new RegExp("\n", "g"), "<br>")
-      })
-  }
- 
+      document.getElementById("result").innerHTML = data.lyrics.replace(new RegExp("\n", "g"), "<br>")
 
-    // var queryURL = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=&key=" + APIKey + "AIzaSyARmz0JqhDmVgCCye_QcvXE8D7yzm4cKvk"
+    })
+}
 
-    // $.ajax({
-    //         url: queryURL,
-    //         method: "GET"
-    //     }).then(function (response) {
 
-    //         console.log(queryURL);
+// var queryURL = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=&key=" + APIKey + "AIzaSyARmz0JqhDmVgCCye_QcvXE8D7yzm4cKvk"
 
-    //         var videoId = response.items[0].id.videoId
-    //         console.log(videoId)
+// $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     }).then(function (response) {
 
-    //         var youTubeVideoURL = `https://www.youtube.com/watch?v=${videoId}`
+//         console.log(queryURL);
 
-    //         console.log(youTubeVideoURL)
-    //     })
-    // })
+//         var videoId = response.items[0].id.videoId
+//         console.log(videoId)
+
+//         var youTubeVideoURL = `https://www.youtube.com/watch?v=${videoId}`
+
+//         console.log(youTubeVideoURL)
+//     })
+// })
--- assets/JS/player.js
+++ assets/JS/player.js
@@ -1,27 +1,29 @@
 $(document).ready(function () {
-            console.log('ready!')
+        console.log('ready!')
 
 
-            var queryURL = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=&key=" + APIKey + "AIzaSyARmz0JqhDmVgCCye_QcvXE8D7yzm4cKvk"
+        var queryURL = "https://youtube.googleapis.com/youtube/v3/search?part=snippet&q=&key=" + APIKey + "AIzaSyARmz0JqhDmVgCCye_QcvXE8D7yzm4cKvk"
 
 
-            $.ajax({
-                    url: queryURL,
-                    method: "GET"
+        $.ajax({
+                url: queryURL,
+                method: "GET"
                 }).then(function (response) {
-
                     console.log(queryURL);
+                    
+        var videoId = response.items[0].id.videoId
+            console.log(videoId)
 
-                    var videoId = response.items[0].id.videoId
-                    console.log(videoId)
+        var youTubeVideoURL = `https://www.youtube.com/watch?v=${videoId}`
+            console.log(youTubeVideoURL)
 
-                    var youTubeVideoURL = `https://www.youtube.com/watch?v=${videoId}`
-
-                    console.log(youTubeVideoURL)
-
-                    $("#display-div").html(`<a href=${youTubeVideoURL}> Youtube Link Generated form API</a>`);
-                })
+        $("#display-div").html(`<a href=${youTubeVideoURL}> Youtube Link Generated form API</a>`);
             })
+        })
+
+        var ifreame = ("#ytframe").append 
+            
+
 
             // to get iframe back, going to need function before this
             // style.disply=block

f7c7cd1cf6dcf62ccd001323b15b7a2d72aae952
Yasmim Sampaio <yasmimsampaio10@hotmail.com>
2020-11-13 15:41:21 -0500

    added the working API

--- assets/JS/instruments.js
+++ assets/JS/instruments.js
@@ -1,13 +1,26 @@
-// var searchBtn = document.getElementById("button");
-//     console.log(searchBtn);
-function searchSong() {
-    // var song = $("#song").val();
-    // console.log(song);
-    var searchSong = $("#searchSong").val();
-    console.log(searchSong);
+var artist;
+var query = "https://genius.p.rapidapi.com/search?q=" + artist;
+console.log(query);
 
+function searchArtist() {
+    artist = $("#searchArtist").val();
+    console.log(artist);
+    const settings = {
+        "async": true,
+        "crossDomain": true,
+        "url": "https://genius.p.rapidapi.com/search?q=" + artist,
+        "method": "GET",
+        "headers": {
+            "x-rapidapi-key": "5d9a59c115msh16ea22a2f0e36abp1b797ejsnd22573626960",
+            "x-rapidapi-host": "genius.p.rapidapi.com"
+        }
+    };
+    
+    $.ajax(settings).done(function (response) {
+        console.log(response);
+    });
 }
-// searchBtn.addEventListener("click", searchSong)
 
-$("#button").on("click", searchSong);
+$("#searchBtn").on("click", searchArtist);
+
 

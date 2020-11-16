91e5ec0c0cbf02dbd4bc0012aa0cd430eb5ddad8
accubility <joshua.obando98@gmail.com>
2020-11-15 21:59:04 -0500

    forgot to add html link to all html nav bars

--- assets/JS/ytsearch.js
+++ assets/JS/ytsearch.js
@@ -1,4 +1,19 @@
 $(document).ready(function(){
 
-    var = 
+    var APIKey = "AIzaSyABwqMY0kKtIekrntpiA99fXdYNeF-eTvI"
+
+    $("form").submit(function(event){
+        event.preventDefault()
+
+        var search = $("#search").val()
+
+
+        videoSearch(APIKey,search, 1)
+    })
+
+    function videoSearch(key,search,maxResults){
+        $.get("https://www.googleapis.com/youtube/v3/search?key=" + key + "&type=video&part=snippet&maxResults=" + maxResults + "&q" + search,function(data){
+            console.log(data)
+        })
+    }
 })
